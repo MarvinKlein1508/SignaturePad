e2acebd0ddf3a6dfcf054a9c7ccaf6d1a2ac50f5
Marvin Klein <marvin.klein@harold-scholz.de>
2023-04-11 08:50:40 +0200

    Update sample projects

---
+++ BlazorServerTests/MyInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace BlazorServerTests
+{
+    public class MyInput
+    {
+        public byte[] Signature { get; set; } = Array.Empty<byte>();
+        public string SignatureAsBase64 => Encoding.UTF8.GetString(Signature);
+    }
+
+
+}
---
+++ BlazorWebassemblyTests/MyInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace BlazorWebassemblyTests
+{
+    public class MyInput
+    {
+        public byte[] Signature { get; set; } = Array.Empty<byte>();
+        public string SignatureAsBase64 => Encoding.UTF8.GetString(Signature);
+    }
+}
